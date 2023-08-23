fb4a4f3be12a6573211a4f8ea4ce984ce5434f4e
Adeoye Adefolaju <adefolajuadeoye15@gmail.com>
2023-08-23 10:14:38 +0100

    search store mobile responsiveness

--- src/pages/search_stores/search_stores.js
+++ src/pages/search_stores/search_stores.js
@@ -2,6 +2,10 @@ import "../../css/main.css"
 import "./search_stores.css"
 import Head from "../../components/header/Head"
 import StoreImg from "../../Img/Rectangle 375.png"
+import MobileBottomNav2 from "../../Components/mobile_bottom_nav2";
+import MobileSearchType from "../../Components/mobile_search_type";
+import Logo from "../../Img/farmylogo.png"
+import Ratings from "../../Img/Full Four Stars.png"
 
 const SearchStore = () =>{
     return<>
@@ -252,6 +256,49 @@ const SearchStore = () =>{
         </div>
     </div>
     
+
+
+      
+    {/* -------MOBILE VIEW---- */}
+        
+    <div className="mobile-view">
+           
+           <div className="search-store-mobile-body">
+           <div className="Logo">
+           <img className="" src={Logo}/>
+           </div>
+           <MobileSearchType/>
+           <div className="mobile-search-bar">
+               <div className="mobile-search-bar-filter">
+                   <p>Select Category</p>
+                   <i className="bx bx-chevron-down"></i>
+               </div>
+               <input PlaceHolder="...search product" className="mobile-search-bar-input-field"/>
+               <i class="bx bx-search-alt-2"></i>
+           </div>
+           <div className="search-store-mobile-container">
+               <div className="search-store-mobile-container-box">
+                   <div className="search-store-mobile-container-box-img">
+                    <img src={StoreImg} alt="img"/>
+                   </div>
+                   <div className="search-store-mobile-container-box-name">
+                       <h3>Foodco Stores</h3>
+                       <p className="search-store-mobile-container-box-name-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sodales sit senectus vel turpis tincidunt nec eget maecenas. Habitant eget viverra orci tellus libero odio pulvinar sed pellentesque.</p>
+                   </div>
+                   <div className="ratings3"><img  src={Ratings}  alt="img"/> <p>4.8</p></div>
+                   <div className="search-store-mobile-container-box-miles">
+                        <h6>About <br/>40km Away </h6>
+                   </div>
+               </div>
+           </div>
+           </div>
+               <MobileBottomNav2/>
+       </div>
+
+
+
+
+
     </>
 };
 export default SearchStore;
